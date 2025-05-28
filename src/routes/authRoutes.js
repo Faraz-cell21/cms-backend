083a9626668a105ba1d7f41e253326c0b4714e3f
Faraz-cell21 <faraz127200@outlook.com>
2025-05-28 09:42:20 +0500

    authRoute

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const { login, logout } = require('../controllers/authController');
 const router = express.Router();
 
-// POST /api/auth/login
 router.post('/login', login);
 
-// GET /api/auth/logout
 router.get('/logout', logout);
 module.exports = router;
